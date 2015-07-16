1c48d8acada8f5a4f8641646d8268e6d1bd8def9
Jaafar BENNANI-SMIRES <jaafar.bennani-smires@microsfair.com>
2015-07-16 11:03:12 +0200

    Fix

--- src/main/webapp/frontend/assets/js/specifics/mod-sendmail-contact.js
+++ src/main/webapp/frontend/assets/js/specifics/mod-sendmail-contact.js
@@ -35,7 +35,7 @@ $(function () {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
                     $('#success > .alert-success')
-                            .append("<strong>SUCCEESS</strong>");
+                            .append("<strong>SUCCESS</strong>");
                     $('#success > .alert-success')
                             .append('</div>');
 
